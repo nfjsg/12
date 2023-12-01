63e2429aa8ac549dd97d7df75d1f116b2d3c7229
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:33:36 -0500

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,11 @@
+// Middleware to check if the user is logged in
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    // Redirect to login if not logged in
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
 
+module.exports = withAuth;
