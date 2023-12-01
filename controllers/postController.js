92a3fe581bceb92f6406985b0c029d785a8f9965
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:28:00 -0500

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,38 @@
 
+const express = require('express');
+const router = express.Router();
+const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Single post route - Get a specific blog post and its comments
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User },
+        {
+          model: Comment,
+          include: [{ model: User }],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Other post route handlers as needed
+
+module.exports = router;
