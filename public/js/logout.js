a89761dd4570a556bc74a2ab2d8d6e219567a7ea
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:32:17 -0500

    Update logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,23 @@
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Logout function
+  const logoutHandler = async () => {
+    try {
+      const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Logout failed. Please try again.');
+      }
+    } catch (err) {
+      console.error('Error during logout:', err);
+    }
+  };
+
+  // Event listener for logout button
+  document.querySelector('#logout').addEventListener('click', logoutHandler);
+});
