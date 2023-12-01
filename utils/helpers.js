6ff58ed4322c17ba9c2ee9d5d9da8a17f693c419
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:33:57 -0500

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,27 @@
 
+const helpers = {
+  // Format date as MM/DD/YYYY
+  format_date: (date) => {
+    return date.toLocaleDateString();
+  },
+
+  // Format large numbers with commas
+  format_amount: (amount) => {
+    return parseInt(amount).toLocaleString();
+  },
+
+  // Get a random emoji
+  get_emoji: () => {
+    const randomNum = Math.random();
+
+    if (randomNum > 0.7) {
+      return `<span for="img" aria-label="lightbulb">💡</span>`;
+    } else if (randomNum > 0.4) {
+      return `<span for="img" aria-label="laptop">💻</span>`;
+    } else {
+      return `<span for="img" aria-label="gear">⚙️</span>`;
+    }
+  },
+};
+
+module.exports = helpers;
