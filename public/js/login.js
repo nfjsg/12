661ccc2163ab1a9f319e6136a08cd57c209a7545
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:31:50 -0500

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,31 @@
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Login form handler
+  const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+      try {
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Login failed. Please check your credentials and try again.');
+        }
+      } catch (err) {
+        console.error('Error during login:', err);
+      }
+    }
+  };
+
+  // Event listener for login form
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+});
