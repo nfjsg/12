f177a63c1b1493bd6195dbf25b14bda14d228afd
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:39:05 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS tech_blog_db;
+
+-- Create a new database
+CREATE DATABASE tech_blog_db;
+
+-- Switch to the new database
+USE tech_blog_db;
+
+-- Define your tables, constraints, and relationships here
+-- ...
 
