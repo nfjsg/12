d0b129c488c943dd980e9f13f23b0f0900689e39
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:38:30 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,24 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // JawsDB connection for deployment
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Local MySQL connection
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
 
