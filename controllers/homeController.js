4bbe0c73670e896f3428436b49f63ad512c33996
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:27:34 -0500

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,1 +1,27 @@
 
+const express = require('express');
+const router = express.Router();
+const { Post, User } = require('../models');
+
+// Home route - Get all blog posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }],
+      order: [['createdAt', 'DESC']],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Other homepage route handlers as needed
+
+module.exports = router;
