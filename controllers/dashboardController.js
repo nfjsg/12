6a93fab1e48e63263c1268de2ca66cd4bf3b8ffd
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-12-01 18:27:12 -0500

    Update dashboardController.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,1 +1,35 @@
 
+const express = require('express');
+const router = express.Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Dashboard route - Get all user's posts
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add new blog post route
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    logged_in: true,
+  });
+});
+
+// Other dashboard route handlers as needed
+
+module.exports = router;
